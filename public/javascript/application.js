7a3f44e52f6d7b8d45c13c12cf4278d5a7b9c0f0
marcoaam <marco.army@gmail.com>
2014-06-23 20:15:21 +0100

    seccond version

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,3 +2,31 @@ $(document).on('click', '#go-search', function() {
 
 	findImagesOnGoogle({keywords: $('#search-term').val(), container:'#search-results'})
 });
+
+$(document).on('click', '#search-results img', function() {
+  var url = $(this).data('url');
+  $("#workspace img").remove();
+  var img = $("<img>").attr('src', url);
+  $("#workspace").append(img);
+});
+
+$(document).on('input', '#text', function() {
+	$("#caption").text($(this).val());
+});
+
+$(document).on('change', '#left', function() {
+	$("#caption").css("left", $(this).val() + 'px');
+});
+$(document).on('change', '#width', function() {
+	$("#caption").css("width", $(this).val() + 'px');
+});
+$(document).on('change', '#size', function() {
+	$("#caption").css("font-size", $(this).val() + 'px');
+});
+$(document).on('change', '#colour', function() {
+	$("#caption").css("color", $(this).val());
+});
+$(document).on('change', '#align', function() {
+	$("#caption").css("text-align", $(this).val());
+});
+
